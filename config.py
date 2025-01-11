a4e01af03336b7fb71bf76bdb45bc2f1faaf6a87
Artists <arists228.grogy@gmail.com>
2025-01-11 23:01:21 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # Замените 'YOUR_BOT_TOKEN' на токен вашего бота
-YOUR_BOT_TOKEN = "6722986317:AAFEPHtSUaBadgshWjp6_mXMHBBC7yYp-Yw"
+YOUR_BOT_TOKEN = ""
 #Замените 123456789 на ID администратора группы.
-ID_CHAT_ADMIN = "862347966"
+ID_CHAT_ADMIN = ""
